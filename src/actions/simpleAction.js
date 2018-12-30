ef6c03b3012e7d7199831d8cf65fd83d9ebfb55c
marycodes2 <mbeliveau92@gmail.com>
2018-12-30 18:23:58 -0500

    fixed issue in action

--- src/actions/simpleAction.js
+++ src/actions/simpleAction.js
@@ -6,7 +6,7 @@ function fetchedMyCards(cardData){
   return {type: "FETCH_MY_CARDS", cardData}
 }
 
-BASE_URL = 'https://sleepy-ravine-75459.herokuapp.com/api/v1/'
+let BASE_URL = 'https://sleepy-ravine-75459.herokuapp.com/api/v1/'
 
 function fetchCards(data) {
   if (data.user) {
